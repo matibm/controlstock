ae9b91b7ed8341574c02c09fe87be7d60a27dc89
unknown <kewel2010.mb@gmail.com>
2020-11-26 12:31:30 -0300

    conneccion base de datos

--- app.js
+++ app.js
@@ -88,8 +88,8 @@ app.use(express.static(`../inventario-frontend/dist/control-stock`));
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname + `/../inventario-frontend/dist/control-stock/index.html`));
 });
-
-mongoose.connection.openUri('mongodb://localhost:27017/Productos', (err, res) => {
+// 'mongodb+srv://matibm:rb433ah01@cluster0-pywni.mongodb.net/Productos?retryWrites=true&w=majority'
+mongoose.connection.openUri('mongodb+srv://matibm:rb433ah01@cluster0-pywni.mongodb.net/Productos?retryWrites=true&w=majority', (err, res) => {
         if (err) throw err;
         console.log("Base de datos:  \x1b[32m%s\x1b[0m", ' online');
         open('http://localhost:3000');
